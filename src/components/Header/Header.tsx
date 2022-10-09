5e5edf83108899497b62e476998edbd657c36df9
Marcos Sugai Ogama <pazzino2000@yahoo.it>
2022-10-09 09:43:55 -0300

    Tablet custom 20221009

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,10 @@ import "./Header.css"
 function Header() {
     return(<>
     <header className="header">
-        <img src="../src/assets/img/Vector.svg" alt="Logo da Alura Geek" className="header__logo" />
-        <img src="../src/assets/img/AluraGeek.svg" alt="Imagem do Texto da Alura Geek" className="header__text" />
+        <div className="header__logotext">
+            <img src="../src/assets/img/Vector.svg" alt="Logo da Alura Geek" className="header__logo" />
+            <img src="../src/assets/img/AluraGeek.svg" alt="Imagem do Texto da Alura Geek" className="header__text" />
+        </div>
         <a href="#" className="header__button">Login</a>
         <img src="../src/assets/img/Search.svg" alt="Imagem busca" className="header__search" />
     </header>
